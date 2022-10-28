e13603ff4e36603ac675c816fd3bca8bb13f8d02
tony5768 <stoyanzlatev22@gmail.com>
2022-10-28 18:06:28 +0300

    Create Stores Model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Promotion
+
+admin.site.register(Product)
+admin.site.register(Promotion)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
+class Promotion(models.Model):
+    store = models.ForeignKey("stores.Store", on_delete=models.CASCADE, related_name='promotions')
+    expire_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.store} {self.expire_date}"
+
+
 class Product(models.Model):
+    promotion = models.ForeignKey(Promotion, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
